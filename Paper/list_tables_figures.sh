993433d127b5e80e6f7ccf4546858bff06e60cd8
jamiefogel <jamie.fogel@gmail.com>
2023-05-11 10:14:46 -0400

    Update list_tables_figures.sh
    Now inputting the file to be searched is mandatory but an output file is optional. if not specified then will just print to command line.

--- Paper/list_tables_figures.sh
+++ Paper/list_tables_figures.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-  echo "Please provide the input filename and output filename as arguments."
+if [ $# -lt 1 ]; then
+  echo "Please provide the input filename as an argument."
   exit 1
 fi
 
 tex_file="$1"
-output_file="$2"
 
-tex_file="$1"
+# Extract the list of files
+file_list=$(egrep -o '\\input\{[^}]+\}|\\includegraphics(?:\[[^]]+\])?\{[^}]+\}' "$tex_file" | sed -E 's/.*\{(.*)\}.*/\1/')
 
-# Extract the filepath from \def\figures{}
-figures_path=$(grep -oE '\\def\\figures\{[^}]+\}' "$tex_file" | sed -E 's/.*\{(.*)\}.*/\1/')
-
-# Extract the list of files and replace \figures with the actual filepath
-file_list=$(egrep -o '\\input\{[^}]+\}|\\includegraphics(?:\[[^]]+\])?\{[^}]+\}' "$tex_file" | sed -E -e 's/.*\{(.*)\}.*/\1/')
-
-# Write the file list to a text file
-echo "$file_list" > "$output_file"
-
-
-#-e '/^\\def\\figures\{.*\}/{s/.*\{(.*)\}.*/\1/; h;}' -e "/\\includegraphics/{s/\\includegraphics(\[.*\])?\{(.*)\}/\2/; x; G; s/(.*)\n(.*)/$figures_path\/\2/;"
+# Check if output file is provided
+if [ $# -eq 2 ]; then
+  output_file="$2"
+  echo "$file_list" > "$output_file"
+else
+  echo "$file_list"
+fi
