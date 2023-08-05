38a6e0b9ee5309c6aacde40ef11e6c2251a55bd1
Jamie Fogel <jamie.fogel@gmail.com>
2023-08-05 12:02:22 -0300

    Got the concentration_figures() function working

--- Code/concentration_figures.py
+++ Code/concentration_figures.py
@@ -6,6 +6,9 @@ Created on Mon Oct  4 14:04:06 2021
 @author: jfogel
 """
 
+from matplotlib.collections import LineCollection
+
+
 ################
 ## Make into function or loop and then incorporate occ2Xmeso
 ##################
@@ -16,23 +19,67 @@ data_full = pd.read_csv(mle_data_filename)
 data_full = data_full.loc[(data_full.iota!=-1) & (data_full.gamma!=-1)]
 
 
-xvar = 'iota'
-yvarlist = ['sector_IBGE','gamma']
-yvarlabels = {'sector_IBGE':'Sector','gamma':'Market'}
+def concentration_figures(xvar, xvarlabel, yvarlist, yvarlabels):
+    # Example usage: 
+    #xvar = 'iota'
+    #xvarlabel = 'Workers'
+    #yvarlist = ['sector_IBGE','gamma']
+    #yvarlabels = {'sector_IBGE':'Sector','gamma':'Market'}
+    fig, ax = plt.subplots()
+    savename_str = ''
+    for yvar in yvarlist:
+        print(yvar)
+        crosstab = pd.crosstab(index = data_full[xvar], columns = data_full[yvar])
+        yvar_probabilities_by_xvar = crosstab.div(crosstab.sum(axis=1),axis=0).reset_index()
+        yvar_probabilities_by_xvar['hhi'] = yvar_probabilities_by_xvar.drop(columns=xvar).pow(2).sum(axis=1)
+        xvar_counts = data_full[xvar].value_counts().reset_index().rename(columns={'index':xvar,xvar:'count'})
+        # Normalize the counts by the minimum count and round to an integer. This avoids having so many points in the scatter plot that it won't print.
+        xvar_counts['count'] = np.round(xvar_counts['count']/xvar_counts['count'].min())
+        yvar_probabilities_by_xvar = yvar_probabilities_by_xvar.merge(xvar_counts, on=xvar)
+        weighted_hhis = yvar_probabilities_by_xvar.loc[yvar_probabilities_by_xvar.index.repeat(yvar_probabilities_by_xvar['count'])]['hhi'].sort_values()
+        ax.scatter(np.arange(1,weighted_hhis.shape[0]+1), weighted_hhis,s=3,label=yvarlabels[yvar])
+        del crosstab, yvar_probabilities_by_xvar, xvar_counts, weighted_hhis
+        savename_str = savename_str + '_' + yvar
+    ax.set_xlabel(xvarlabel + ' (sorted by employment HHI)')
+    ax.set_ylabel('Concentration (HHI)')
+    ax.set_xticklabels([])
+    ax.set_xticks([])
+    ax.set_ylim(0,1)
+    ax.legend()
+    ax.figure.savefig(figuredir+fname_stub+'_'+xvar+savename_str+'_test.png', dpi=300,bbox_inches='tight') # Used by paper and slides
+
+concentration_figures('iota', 'Workers', ['sector_IBGE','gamma'], {'sector_IBGE':'Sector','gamma':'Market'})
+concentration_figures('iota', 'Workers', ['clas_cnae20','gamma'], {'clas_cnae20':'5-Digit Industry','gamma':'Market'})
+concentration_figures('iota', 'Workers', ['occ2Xmeso','gamma'], {'occ2Xmeso':'Occ2 X Meso Region','gamma':'Market'})
+
+
+
+
 
 fig, ax = plt.subplots()
-dict = {}
+
 for yvar in yvarlist:
     print(yvar)
-    crosstab = pd.crosstab(index = data_full[xvar], columns = data_full[yvar])
-    yvar_probabilities_by_xvar = crosstab.div(crosstab.sum(axis=1),axis=0).reset_index()
+    crosstab = pd.crosstab(index=data_full[xvar], columns=data_full[yvar])
+    yvar_probabilities_by_xvar = crosstab.div(crosstab.sum(axis=1), axis=0).reset_index()
     yvar_probabilities_by_xvar['hhi'] = yvar_probabilities_by_xvar.drop(columns=xvar).pow(2).sum(axis=1)
-    xvar_counts = data_full[xvar].value_counts().reset_index().rename(columns={'index':xvar,xvar:'count'})
+    xvar_counts = data_full[xvar].value_counts().reset_index().rename(columns={'index': xvar, xvar: 'count'})
     yvar_probabilities_by_xvar = yvar_probabilities_by_xvar.merge(xvar_counts, on=xvar)
-    weighted_hhis = yvar_probabilities_by_xvar.loc[yvar_probabilities_by_xvar.index.repeat(yvar_probabilities_by_xvar['count'])]['hhi'].sort_values()
-    ax.scatter(np.arange(1,weighted_hhis.shape[0]+1), weighted_hhis,s=3,label=yvarlabels[yvar])
-    #del crosstab, yvar_probabilities_by_xvar, xvar_counts, weighted_hhis
+    # Sort the values
+    weighted_hhis_sorted = yvar_probabilities_by_xvar['hhi'].sort_values()
+    # List to hold the line segments
+    lines = []
+    x_values = np.arange(1, weighted_hhis_sorted.shape[0] + 1)
+    for x, y, count in zip(x_values, weighted_hhis_sorted, yvar_probabilities_by_xvar['count']):
+        # Compute the width based on the count
+        width = count / 100  # Adjust the division factor to control the width
+        # Append the line segment as (x1, y1), (x2, y2) to the lines list
+        lines.append([(x - width, y), (x + width, y)])
+    # Create a LineCollection from the lines and add to the axes
+    lc = LineCollection(lines, color='blue') # Adjust color as needed
+    ax.add_collection(lc)
 
+    
 ax.set_xlabel('Workers (sorted by employment HHI)')
 ax.set_ylabel('Concentration (HHI)')
 ax.set_xticklabels([])
@@ -40,6 +87,12 @@ ax.set_xticks([])
 ax.set_ylim(0,1)
 ax.legend()
 ax.figure.savefig(figuredir+fname_stub+'_iota_gamma_sector_hhi_worker_weighted_test.png', dpi=300,bbox_inches='tight') # Used by paper and slides
+
+# Rest of the code...
+
+
+# Rest of the code...
+
 
 
 # Issue: the way I'm doing the weighting is running out of memory
