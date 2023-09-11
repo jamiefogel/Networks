fe11e267ca92858e92ed209068a01fb7bf8205f2
Jamie Fogel <jamie.fogel@gmail.com>
2023-09-10 21:00:51 -0300

    Add print to log

--- Code/run_do_all.sh
+++ Code/run_do_all.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#!/bin/bash
-
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 directory="../Logs/"
 nohup python3 -u do_all.py > "${directory}/do_all_${timestamp}.log" 2>&1 &
 PYTHON_PID=$!
 echo "Linux Process ID of python3 script: $PYTHON_PID" 
-#echo "Linux Process ID of python3 script: $PYTHON_PID" | tee -a "${directory}/do_all_${timestamp}.log"
+echo "Linux Process ID of python3 script: $PYTHON_PID" >> "${directory}/do_all_${timestamp}.log"
