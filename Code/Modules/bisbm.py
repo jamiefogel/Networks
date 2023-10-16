eb6d9aeb49f14bc1b1ea4afd489d1a11ccd6f82d
Jamie Fogel <jamie.fogel@gmail.com>
2023-10-16 19:39:25 -0300

    Add a MCMC sweeps function to the bisbm class

--- Code/Modules/bisbm.py
+++ Code/Modules/bisbm.py
@@ -15,17 +15,18 @@ import datetime
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import pickle
 import re
 
 class bisbm():
-# Need to indent everything below this    
+# Need to indent everything below this
     def __init__(self):
         self.g = None ## network
         self.edgelist = pd.DataFrame()
         self.edgelist_w_blocks = pd.DataFrame()
         self.state = None
+        self.state_mcmc = None
         self.num_jobs = np.nan
         self.num_workers = np.nan
         self.num_edges = np.nan
@@ -35,7 +36,7 @@ class bisbm():
         self.alphas = pd.DataFrame()
 
 
-        
+
 
     #################################################################################
     # Simply load an edgelist and possibly allow for restrictions on workers per job
@@ -53,16 +54,16 @@ class bisbm():
             nw = self.edgelist['wid'].nunique()
             edges = self.edgelist.shape[0]
             print(nw, 'unique workers,',nj, 'unique jobs, and', edges, 'edges in raw data' )
-            
+
             # Count the number of workers per job and jobs per worker
             self.edgelist['workers_per_job'] = self.edgelist.groupby('jid')['jid'].transform('count')
             self.edgelist['jobs_per_worker'] = self.edgelist.groupby('wid')['wid'].transform('count')
-    
+
             # Optionally, drop jobs with few workers
             if min_workers_per_job is not None:
                   indexNames = self.edgelist[ (self.edgelist['workers_per_job'] < min_workers_per_job ) ].index
                   self.edgelist.drop(indexNames , inplace=True)
-    
+
             # Create new worker and job ID variables with jobs running from 0 to num_jobs-1 and workers running from num_jobs to (num_jobs+num_workers-1). This allows me to specify the bipartite structure more easily
             self.num_jobs    = self.edgelist['jid'].nunique()
             self.num_workers = self.edgelist['wid'].nunique()
@@ -70,7 +71,7 @@ class bisbm():
             self.edgelist['jid_py']=self.edgelist.groupby(['jid']).ngroup()
             self.edgelist['wid_py']=self.edgelist.groupby(['wid']).ngroup() + self.num_jobs
             print(self.num_workers, 'unique workers,',self.num_jobs, 'unique jobs, and', self.num_edges, 'edges in graph after restricting to at least', min_workers_per_job, 'workers per job' )
-            
+
             g = gt.Graph(directed=False)
             g.add_edge_list(self.edgelist[['jid_py','wid_py']].to_numpy().astype('float64')  )
 
@@ -107,7 +108,7 @@ class bisbm():
     #################################################################################
     def fit(self,overlap = False, hierarchical = True, n_init = 1):
         '''
-        Fit the bisbm 
+        Fit the bisbm
         - overlap, bool (default: False). Overlapping or Non-overlapping groups.
             Overlapping not implemented yet
         - hierarchical, bool (default: True). Hierarchical SBM or Flat SBM.
@@ -131,7 +132,7 @@ class bisbm():
             ## the inference
             ## JSF: I think this loops over a bunch of initial conditions in order to pick the one with the best (smallest) minimum description length. This is to avoid finding local minima
             starttime = datetime.datetime.now()
-            print("Starting BiSBM estimation block at ", datetime.datetime.now())                     
+            print("Starting BiSBM estimation block at ", datetime.datetime.now())
             mdl = np.inf ##
             for i_n_init in range(n_init):
                 state_tmp = gt.minimize_nested_blockmodel_dl(g, state_args=state_args)
@@ -139,14 +140,14 @@ class bisbm():
                 if mdl_tmp < mdl:
                     mdl = 1.0*mdl_tmp
                     state = state_tmp.copy(sampling=True)
-            
 
-                    
+
+
             print("BiSBM estimation completed at ", datetime.datetime.now())
             print("Estimation time: ", datetime.datetime.now()-starttime)
 
             # In a previous version I printed the number of worker and job blocks. This relied on code from sbmtm.get_groups() that I could add back in
-                    
+
             self.state = state
             ## minimum description length
             self.mdl = state.entropy()
@@ -157,7 +158,7 @@ class bisbm():
             if L == 2:
                 self.L = 1
                 print("Found only trivial bipartite structure")
-                
+
             ## omit trivial levels: l=L-1 (single group), l=L-2 (bipartite)
             else:
                 self.L = L-2
@@ -182,7 +183,7 @@ class bisbm():
             mlevel = self.L
         else:
             mlevel = max_level+1
-            
+
         for l in range(mlevel):
             # I don't know why copy=True is necessary but without it some of the 0s get converted to very large numbers for some reason
             #Old version: temp = pd.DataFrame(self.state.project_level(l).get_blocks().a, columns=['blocks_level_'], copy=True)
@@ -199,8 +200,8 @@ class bisbm():
             temp = temp.rename(columns=lambda cname: cname + str(l))
             df = pd.concat([df,temp], axis=1)
 
-            del temp        
-        
+            del temp
+
 
         job_blocks    = df[df['worker_node']==0].drop(columns=['worker_node']).rename(columns={"id":"jid","id_py":"jid_py"}).rename(columns=lambda cname: "job_"+cname if (cname!="jid_py" and cname!="jid") else cname )
         worker_blocks = df[df['worker_node']==1].drop(columns=['worker_node']).rename(columns={"id":"wid","id_py":"wid_py"}).rename(columns=lambda cname: "worker_"+cname if (cname!="wid_py" and cname!="wid") else cname )
@@ -211,7 +212,7 @@ class bisbm():
 
         self.num_job_blocks = num_job_blocks
         self.num_worker_blocks = num_worker_blocks
-        
+
         if output is not None:
             if output.endswith(".csv"):
                 edgelist_w_blocks.to_csv(output, index=False)
@@ -249,29 +250,44 @@ class bisbm():
             return -1
 
         block_var = 'worker_blocks_level_' + str(level)
-        	
+
         worker_blocks = self.edgelist_w_blocks[['wid',block_var]].drop_duplicates().rename(columns={block_var:'iota'})
-        
+
         # From SAS
         df = pd.read_csv(earnings_file, usecols=['wid',earnings_var])
-        
+
         df2 = df.merge(worker_blocks, on='wid',validate='m:1', indicator='True')
-        
+
         # df2 = df.merge(job_blocks, how='left', on='jid',validate='m:1', indicator='True')
         # df2['True'].value_counts()
         # 3.6 million matched, 1.9 million not matched for Rio. My guess is that this is because we dropped jids with fewer than 10 workers
 
         iota_mean_earnings = df2.groupby(['iota'])[earnings_var].mean().to_frame().reset_index().rename(columns={earnings_var:"mean_earnings"})
-        
+
         if output is not None:
             iota_mean_earnings.to_csv(output, index=False)
-            
+
         self.iota_mean_earnings=iota_mean_earnings
 
 #model_rio.compute_alphas(level=2, earnings_file='./data/edges_rio_occ4_indv_data_10.csv', earnings_var='salario', output='./data/rio_alphas_level_2.csv')
 
 
+    def mcmc_sweeps(self, savefile, tempsavedir='./', numiter=1000, seed=734):
+        print('Starting MCMC sweeps at ', datetime.datetime.now())
+        gt.seed_rng(seed)
+        self.state_mcmc = self.state.copy()
+        
+        entropy = []
+        entropy.append(self.state_mcmc.entropy())
 
+        t0 = datetime.datetime.now()
 
-
-
+        for i in range(numiter): # this should be sufficiently large
+            print(i)
+            self.state_mcmc.multiflip_mcmc_sweep(beta=np.inf, niter=5)
+            entropy.append(self.state_mcmc.entropy())
+            pickle.dump(entropy, open(tempsavedir + 'entropy.p', "wb" ) )
+            print("Improvement: ", round(((entropy[0] - entropy[-1])/entropy[0])*100,4))
+            print("Time spent: ", datetime.datetime.now()-t0)
+            pickle.dump([self.state_mcmc,i,entropy], open(tempsavedir + 'tmp_state_mcmc_iters.p', "wb" ), protocol = 4)
+            os.rename(tempsavedir + 'tmp_state_mcmc_iters.p', savefile)
