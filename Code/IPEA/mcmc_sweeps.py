1ca49eabbbb1045ba60af355d0b11cb7aeb93dfc
Jamie Fogel <jamie.fogel@gmail.com>
2023-10-16 19:44:10 -0300

    Edits to obsolete file (replaced by function in bisbm.py)
    Will want to deleted this file eventually but saving it for now in case it's a useful reference

--- Code/IPEA/mcmc_sweeps.py
+++ Code/IPEA/mcmc_sweeps.py
@@ -19,25 +19,29 @@ modelname = '3states_2013to2016'
 #del model
 
 # LOADING THE STATE ALREADY
-state = pickle.load(open('state_mcmc_iters.p', 'rb'))[0]
 
-gt.seed_rng(734)
 
-entropy = []
-entropy.append(state.entropy())
+def mcmc_sweeps(state, numiter=1000, savefile=None, tempfile='tmp_state_mcmc_iters.p', seed=734):
 
-t0 = dt.datetime.now()
-
-for i in range(1000): # this should be sufficiently large
-    print(i)
-    state.multiflip_mcmc_sweep(beta=np.inf, niter=5)
+    gt.seed_rng(seed)
+    
+    entropy = []
     entropy.append(state.entropy())
-    pickle.dump(entropy, open('entropy.p', "wb" ) )
-    print("Improvement: ", round(((entropy[0] - entropy[-1])/entropy[0])*100,4))
-    print("Time spent: ", dt.datetime.now()-t0)
-    pickle.dump([state,i,entropy], open('tmp_state_mcmc_iters.p', "wb" ), protocol = 4)
-    os.rename('tmp_state_mcmc_iters.p','state_mcmc_iters.p')
-
+    
+    t0 = dt.datetime.now()
+    
+    for i in range(numiter): # this should be sufficiently large
+        print(i)
+        state.multiflip_mcmc_sweep(beta=np.inf, niter=5)
+        entropy.append(state.entropy())
+        pickle.dump(entropy, open('entropy.p', "wb" ) )
+        print("Improvement: ", round(((entropy[0] - entropy[-1])/entropy[0])*100,4))
+        print("Time spent: ", dt.datetime.now()-t0)
+        if savefile!=None:
+            pickle.dump([state,i,entropy], open(tempfile, "wb" ), protocol = 4)
+            os.rename(tempfile, savefile)
+        return [state,i,entropy]
+    
 print("printing this just to avoid indentation errors")
 
 #os.chdir('/home/DLIPEA/p13861161/labormkt/labormkt_rafaelpereira/aug2022/data')
@@ -46,3 +50,4 @@ print("printing this just to avoid indentation errors")
 #entropy = pickle.load(open('entropy.p',  "rb" ) )
 #round(((entropy[0] - entropy[-1])/entropy[0])*100,2)
 
+state = pickle.load(open('state_mcmc_iters.p', 'rb'))[0]
