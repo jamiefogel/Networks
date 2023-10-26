7ab03ee6602e42a739674e6aea00351ba054dd4e
Jamie Fogel <jamie.fogel@gmail.com>
2023-10-26 20:24:47 -0300

    Fix filepaths and add submission of parallel_jobtransitions_stack_results.py

--- Code/bash_parallel_jobtransitions_market_based_mutiple_mkts.sh
+++ Code/bash_parallel_jobtransitions_market_based_mutiple_mkts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define an array of parameter values
-cores=27
+cores=26
 cores_list=($(seq 1 ${cores}))
 
 # Get the current date/time
@@ -9,10 +9,14 @@ timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 
 #directory="../../Logs/job_transitions_${timestamp}"
 #mkdir "${directory}"
-directory="../../Logs/job_transitions"
+directory="./Logs/job_transitions"
 
 # Iterate through the array and execute the script with each parameter value
 for c in "${cores_list[@]}"; do
-    nohup python3 parallel_jobtransitions_market_based_multiple_mkts.py "$c" "$cores" > "${directory}/core${c}_${cores}.log" 2>&1 &
+    nohup python3 ./Code/parallel_jobtransitions_market_based_multiple_mkts.py "$c" "$cores" > "${directory}/core${c}_${cores}.log" 2>&1 &
 done
 
+
+wait
+
+nohup python3 ./Code/parallel_jobtransitions_stack_results.py
