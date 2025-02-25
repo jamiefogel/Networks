70dc5d6dd34f8d2a0cea5cd31cb2f2c28e021a57
modenesi <bmodenesi@gmail.com>
2025-02-25 13:08:47 -0300

    Edit SBM estimation code to be function-based and thus more flexible to allow for different specs

--- Code/MarketPower/estimate_sbm_mayara.py
+++ Code/MarketPower/estimate_sbm_mayara.py
@@ -22,113 +22,105 @@ from datetime import datetime
 # 1) LOADING DATA (original function, unchanged)
 # ------------------------------------------------------------------------------
 
+def create_edgelist(firstyear, lastyear):
+    dfs = []
+    # Should this start with 86?
+    for year in range(firstyear, lastyear+1):
+        print(year)
+        df = pd.read_parquet(f'/home/DLIPEA/p13861161/labormkt/labormkt_rafaelpereira/NetworksGit/Code/replicate_mayara/monopsonies/sas/rais{year}.parquet')
+        df = df[['fakeid_worker', 'fakeid_firm', 'fakeid_estab', 'agegroup', 'cbo', 'municipality']]
+        df['state'] =  pd.to_numeric(df['municipality'].astype(str).str[:2], errors='coerce').astype('Int64')
+        # Create jid only for rows where both fakeid_estab and occ4 are not missing
+        df['fakeid_estab'] = df['fakeid_estab'].astype('Int64')
+        df.loc[df['fakeid_estab'].notna() & df['cbo'].notna(), 'jid'] = df['fakeid_estab'].astype(str) + '_' + df['cbo'].astype(str).str[0:4]
+        df.loc[df['fakeid_estab'].isna() | df['cbo'].isna(), 'jid'] = pd.NA
+        dfs.append(df)
 
-dfs = []
-# Should this start with 86?
-for year in range(1986, 1991):
-    print(year)
-    df = pd.read_parquet(f'/home/DLIPEA/p13861161/labormkt/labormkt_rafaelpereira/NetworksGit/Code/replicate_mayara/monopsonies/sas/rais{year}.parquet')
-    df = df[['fakeid_worker', 'fakeid_firm', 'fakeid_estab', 'agegroup', 'cbo', 'municipality']]
-    df['state'] =  pd.to_numeric(df['municipality'].astype(str).str[:2], errors='coerce').astype('Int64')
-    # Create jid only for rows where both fakeid_estab and occ4 are not missing
-    df['fakeid_estab'] = df['fakeid_estab'].astype('Int64')
-    df.loc[df['fakeid_estab'].notna() & df['cbo'].notna(), 'jid'] = df['fakeid_estab'].astype(str) + '_' + df['cbo'].astype(str).str[0:4]
-    df.loc[df['fakeid_estab'].isna() | df['cbo'].isna(), 'jid'] = pd.NA
-    dfs.append(df)
+    stacked = pd.concat(dfs, ignore_index=True)
+    stacked.rename(columns={'fakeid_worker':'wid'}, inplace=True)
+    edgelist = stacked.loc[stacked.jid.notna(),['wid','jid']]
+    edgelist_3states = stacked.loc[(stacked.jid.notna())& (stacked.state.isin([31,33,35])),['wid','jid']]
+    edgelist.to_pickle(root + f'/Data/derived/mayara_edgelist_{firstyear}_{lastyear}.p')
+    edgelist_3states.to_pickle(root + f'/Data/derived/mayara_edgelist_{firstyear}_{lastyear}_3states.p')
+    
+    del edgelist, edgelist_3states, stacked, dfs, df
+    gc.collect()
+    
+    
+    
+def run_sbm_section(modelname, edgelist_path, fit_params=None, run_mcmc=True):
+    """
+    Run the SBM modeling section with the specified modelname and fitting parameters.
+    
+    Parameters:
+        modelname (str): A unique name for the model run (used for output filenames).
+        fit_params (dict, optional): Additional keyword arguments for model.fit.
+        run_mcmc (bool, optional): Whether to run the MCMC sweeps.
+    """
+
+    print('Starting SBM section at', datetime.now())
+    
+    # Create and configure the model
+    model = bisbm.bisbm()
+    model.create_graph(filename=edgelist_path, min_workers_per_job=1, drop_giant=True)
+    
+    # Default fit parameters if none provided
+    if fit_params is None:
+        fit_params = {}
+    model.fit(n_init=1, **fit_params)
+    
+    # Define output paths
+    output_prefix = root + '/Data/derived/sbm_output/'
+    blocks_csv = output_prefix + 'model_' + modelname + '_blocks.csv'
+    jblocks_csv = output_prefix + 'model_' + modelname + '_jblocks.csv'
+    wblocks_csv = output_prefix + 'model_' + modelname + '_wblocks.csv'
+    edgelist_parquet = output_prefix + modelname + '_edgelist_w_blocks.p'
+    model_pickle = output_prefix + 'model_' + modelname + '.p'
+    # Save it before export_blocks() in case something goes wrong 
+    pickle.dump(model, open(model_pickle, "wb"))
+
+    # Export blocks and edgelist with blocks
+    model.export_blocks(output=blocks_csv, joutput=jblocks_csv, woutput=wblocks_csv, max_level=2)
+    model.edgelist_w_blocks[['wid', 'jid', 'workers_per_job', 'jobs_per_worker', 
+                             'jid_py', 'wid_py', 
+                             'job_blocks_level_0', 'job_blocks_level_1', 'job_blocks_level_2', 
+                             'worker_blocks_level_0', 'worker_blocks_level_1', 'worker_blocks_level_2']].to_parquet(edgelist_parquet)
+    
+    # Save the model
+    pickle.dump(model, open(model_pickle, "wb"))
+    print('SBM section complete at', datetime.now())
+    
+    # Optionally run MCMC sweeps
+    if run_mcmc:
+        mcmc_file = output_prefix + 'model_' + modelname + '_mcmc.p'
+        model.mcmc_sweeps(mcmc_file, tempsavedir=output_prefix, numiter=1000, seed=734)
+        pickle.dump(model, open(model_pickle, "wb"))
 
 
-stacked = pd.concat(dfs, ignore_index=True)
-stacked.rename(columns={'fakeid_worker':'wid'}, inplace=True)
-edgelist = stacked.loc[stacked.jid.notna(),['wid','jid']]
-edgelist_3states = stacked.loc[(stacked.jid.notna())& (stacked.state.isin([31,33,35])),['wid','jid']]
-edgelist.to_pickle(root + '/Data/derived/mayara_edgelist_1986_1990.p')
-edgelist_3states.to_pickle(root + '/Data/derived/mayara_edgelist_1986_1990_3states.p')
+edgelist_path = root + '/Data/derived/mayara_edgelist_1986_1990_3states.p'
 
-del edgelist, edgelist_3states, stacked, dfs, df
-gc.collect()
+# Define a list of parameter dictionaries for each SBM run
+sbm_runs = [
+    {
+        'modelname': 'sbm_mayara_1986_1990_3states',
+        'fit_params': {},  # No additional parameters for this run
+        'run_mcmc': True
+    },
+    {
+        'modelname': 'sbm_mayara_1986_1990_3states_7500blocks',
+        'fit_params': {'B_min': 7500, 'B_max': 7500},
+        'run_mcmc': True
+    }
+]
 
+run_create_edgelist = False
+if run_create_edgelist==True:
+    create_edgelist(1986, 1990)
 
-print('Starting SBM section at ', datetime.now())
-modelname = 'sbm_mayara_1986_1990_3states'
-model = bisbm.bisbm()                                                                       
-model.create_graph(filename=root + '/Data/derived/mayara_edgelist_1986_1990_3states.p',min_workers_per_job=1, drop_giant=True)
-model.fit(n_init=1)
-# In theory it makes more sense to save these as pickles than as csvs but I keep getting an error loading the pickle and the csv works fine
-model.export_blocks(output=root + 'Data/derived/sbm_output/model_'+modelname+'_blocks.csv', joutput=root + 'Data/derived/sbm_output/model_'+modelname+'_jblocks.csv', woutput=root + 'Data/derived/sbm_output/model_'+modelname+'_wblocks.csv', max_level=2)
-model.edgelist_w_blocks[['wid', 'jid', 'workers_per_job', 'jobs_per_worker', 'jid_py', 'wid_py', 'job_blocks_level_0', 'job_blocks_level_1', 'job_blocks_level_2', 'worker_blocks_level_0', 'worker_blocks_level_1', 'worker_blocks_level_2']].to_parquet(root + 'Data/derived/sbm_output/'+modelname+'_edgelist_w_blocks.p')
-pickle.dump( model, open(root + 'Data/derived/sbm_output/model_'+modelname+'.p', "wb" ))
-print('SBM section complete at ', datetime.now())
-
-
-
-
-
-'''
-Checking size of components
-
-# Identify the connected components of the (undirected) graph
-comp, hist = gt.label_components(model.g)
-# Find which component is largest
-largest_comp_idx = np.argmax(hist)
-
-in_giant = (comp.a == largest_comp_idx)  # comp.a is the numpy array of indices
-
-fraction_in_giant = in_giant.mean()
-print("Overall fraction of vertices in giant component:", fraction_in_giant)
-kind_array = model.g.vp["kind"].a  # 0 for jobs, 1 for workers
-frac_jobs_in_giant   = in_giant[(kind_array == 0)].mean()
-frac_workers_in_giant = in_giant[(kind_array == 1)].mean()
-
-print("Fraction of job nodes in GC:", frac_jobs_in_giant)
-print("Fraction of worker nodes in GC:", frac_workers_in_giant)
-
-
-# "comp.a" is a numpy array with length == number_of_vertices in model.g
-# job_blocks['jid_py'] indicates the "graph-tool vertex index" for each job
-job_blocks = pd.read_csv(root + 'Data/derived/sbm_output/model_'+modelname+'_jblocks.csv')
-worker_blocks = pd.read_csv(root + 'Data/derived/sbm_output/model_'+modelname+'_wblocks.csv')
-component_index = comp.a  # same length as g.num_vertices()
-job_blocks["component"] = job_blocks["jid_py"].apply(lambda x: component_index[x])
-worker_blocks["component"] = worker_blocks["wid_py"].apply(lambda x: component_index[x])
-# Boolean: is this node in the giant component?
-job_blocks["in_giant"] = (job_blocks["component"] == largest_comp_idx)
-worker_blocks["in_giant"] = (worker_blocks["component"] == largest_comp_idx)
-
-
-jobs_per_block = (
-    job_blocks
-    .groupby("job_blocks_level_0")
-    ["jid_py"]  # or "jid"
-    .count()
-    .rename("count_of_jobs")
-    .reset_index()
-)
-workers_per_block = (
-    worker_blocks
-    .groupby("worker_blocks_level_0")
-    ["wid_py"]
-    .count()
-    .rename("count_of_workers")
-    .reset_index()
-)
-
-jobs_per_block_giant = (
-    job_blocks
-    .query("in_giant == True")
-    .groupby("job_blocks_level_0")
-    ["jid_py"]
-    .count()
-    .rename("count_of_jobs_gc")
-    .reset_index()
-)
-
-jobs_per_block_not_giant = (
-    job_blocks
-    .query("in_giant == False")
-    .groupby("job_blocks_level_0")
-    ["jid_py"]
-    .count()
-    .rename("count_of_jobs_not_gc")
-    .reset_index()
-)
-'''
+# Loop over each run configuration and execute the SBM section
+for run in sbm_runs[1] :
+    run_sbm_section(
+        modelname=run['modelname'],
+        fit_params=run.get('fit_params', None),
+        run_mcmc=run.get('run_mcmc', True)
+    )
